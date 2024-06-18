2470414854d806e25e2b2e235b2419fbd8de8a08
raghav-1998 <raghav.vasdeva@gmail.com>
2024-06-18 13:33:32 +0530

    Some Modification in styling

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,21 +1,27 @@
+import Image from "next/image";
+
 export default function Navbar(){
     return (
-        <header className="fixed-top flex align-items center py-6 ">
-            <div className="w-full mx-auto flex align-items-center justify-between px-3">
-                <div className="flex flex-col">
-                    <h1 className="text-blue-800 text-3xl font-bold">
-                        <a href="/">
-                            <img src="/logo.png" alt="" className="h-screen max-w-full max-h-20 pb-1.25" />
-                            AKSHEYA
-                        </a>
-                    </h1>
+        <header className="fixed top-0 right-0 left-0 flex items-center py-[22px] z-[997] transition-all duration-500 bg-[#fff]">
+            <div className="w-full container mx-auto flex items-center justify-between px-3">
+                <div>
+                    <a href="/" className="flex items-center ">
+                        <Image src="/logo.png" alt="" width={50} height={50} className="pb-[5px] w-auto h-auto"/>
+                        <h1 className="text-blue-800 text-3xl font-bold tracking-[1px]">AKESHYA</h1>
+                    </a>
                 </div>
-                <nav className="relative flex flex-wrap align-items center justify-between">
-                    <ul className="flex flex-row">
-                        <li><a href="" className="py-3 pl-8 text-lg font-normal">Home</a></li>
-                        <li><a href="" className="py-3 pl-8 text-lg font-normal">About</a></li>
-                        <li><a href="" className="py-3 pl-8 text-lg font-normal">Services</a></li>
-                        <li><a href="" className="py-3 pl-8 text-lg font-normal">Contact Us</a></li>
+                <nav className="relative flex flex-wrap items-center justify-between">
+                    <ul className="flex items-center">
+                        <li><a href="" className="py-3 pl-8 text-lg font-normal active:text-[#14279b] transition duartion-300">Home</a></li>
+                        <li><a href="" className="py-3 pl-8 text-lg font-normal active:text-[#14279b] transition duartion-300">About</a></li>
+                        <li><a href="" className="py-3 pl-8 text-lg font-normal active:text-[#14279b] transition duartion-300">Services</a></li>
+                        <li>
+                            <a href="" 
+                                className="ml-[30px] py-[8px] px-[25px] text-lg font-normal active:text-[#14279b] rounded-3xl shadow-[inset_0_0_0_2px_#616467] bg-[#14279b] text-[#fff] transition duartion-300"
+                            >
+                                Contact Us
+                            </a>
+                        </li>
                     </ul>
                 </nav>
             </div>
